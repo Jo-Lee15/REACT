3b109f7cfbc3b059fc3f5944092e5f8d76f6b813
Jo <Jlee.naidoo@gmail.com>
2022-09-09 10:54:29 +0200

    first commit

---
+++ react_beginner_source_code/react-chapter2-master/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My First React App!</h1>
+      </div>
+    );
+  }
+}
+export default App;
