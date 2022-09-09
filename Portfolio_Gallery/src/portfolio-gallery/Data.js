197cb0f5e1c301ddd2006651f63477704a5f14ee
Jo <107405082+Jo-Lee15@users.noreply.github.com>
2022-09-09 12:25:05 +0200

    Add files via upload

---
+++ Portfolio_Gallery/src/portfolio-gallery/Data.js
@@ -0,0 +1,125 @@
+
+const data =[
+    {
+        id:1,
+        image:"./images/html.jpg",
+        title:"HTML Tutorial",
+        description:"Learn HTML! ",
+        category:"frontend",
+       
+    },
+    {
+        id:2,
+        image:"./images/css.jpg",
+        title:"CSS Tutorial",
+        description:"Learn CSS!",
+        category:"frontend",
+       
+    },
+    {
+        id:3,
+        image:"./images/javascript.jpg",
+        title:"JS Tutorial",
+        description:"Learn JavaScript!",
+        category:"frontend",
+       
+    },
+    {
+        id:4,
+        image:"./images/react.jpg",
+        title:"React Js Tutorial",
+        description:"Learn React!",
+        category:"frontend",
+       
+    },
+    {
+        id:5,
+        image:"./images/bootstrap.jpg",
+        title:"Bootstrap Tutorial",
+        description:"Learn Bootstrap!",
+        category:"frontend",
+       
+    },
+    {
+        id:6,
+        image:"./images/php.jpg",
+        title:"PHP Tutorial",
+        description:"Learn PHP!",
+        category:"backend",
+       
+    },
+    {
+        id:7,
+        image:"./images/node-js.jpg",
+        title:"Node.js Tutorial",
+        description:"Learn Node!",
+        category:"backend",
+       
+    },
+    {
+        id:8,
+        image:"./images/asp.jpg",
+        title:"ASP Tutorial",
+        description:"Learn ASP!",
+        category:"backend",
+       
+    },
+    {
+        id:9,
+        image:"./images/sql.jpg",
+        title:"SQL Tutorial",
+        description:"Learn SQL!",
+        category:"database",
+       
+    },
+    {
+        id:10,
+        image:"./images/mongodb.jpg",
+        title:"Mongo DB Tutorial",
+        description:"Learn MongoDB!",
+        category:"database",
+       
+    },
+    {
+        id:11,
+        image:"./images/c.jpg",
+        title:"C Tutorial",
+        description:"Learn C!",
+        category:"programming",
+       
+    },
+    {
+        id:12,
+        image:"./images/c-plus.jpg",
+        title:"C++ Tutorial",
+        description:"Learn C++!",
+        category:"programming",
+       
+    },
+    {
+        id:13,
+        image:"./images/java.jpg",
+        title:"Java Tutorial",
+        description:"Learn Java!",
+        category:"programming",
+       
+    },
+    {
+        id:14,
+        image:"./images/python.jpg",
+        title:"Python Tutorial",
+        description:"Learn Python!",
+        category:"programming",
+       
+    },
+    {
+        id:15,
+        image:"./images/go.jpg",
+        title:"Go Tutorial",
+        description:"Learn Go!",
+        category:"programming",
+       
+    },
+]
+
+export default data;
